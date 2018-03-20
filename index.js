a1671146ba5cb1c3a1de6389524434db49a95719
Elisabeth Narine <elisabethnarine@gmail.com>
2018-03-20 02:55:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function johnLennonFacts (facts) {
   var results = [];
   var count = 0;
   while (count < facts.length) {
-    results.push(facts[count]) + "!!!");
+    results.push(facts[count] + "!!!");
     count ++;
   }
   return results;
